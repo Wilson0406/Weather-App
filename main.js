331207e039dcf71ef2a0773549ada3334857cb18
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-05-16 18:08:04 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+let weather = {
+    "api-key" : "98a048e9d6150d2f201bfcb462abea63",
+    fetchWeather: function (city) {
+        fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q='
+            + city
+            + '&appid=' 
+            + this["api-key"]
+            + "&units=metric"
+            )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));        
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.getElementById("city").innerHTML = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.getElementById("description").innerHTML = description;
+        document.getElementById("temp").innerHTML = temp + "°C";
+        document.getElementById("humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.getElementById("wind").innerHTML = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove('loading');
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.getElementById('search-bar').value);        
+    }
+};
+
+document.getElementById('search-btn').addEventListener('click', function () {
+weather.search();
+});
+
+document.getElementById('search-bar').addEventListener('keyup', function(event) {
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather('Delhi');
